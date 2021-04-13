91ddf1a3cdd7ae1c591832f2eac0e97a720b5e86
kallelampinen <71073159+kallelampinen@users.noreply.github.com>
2021-04-13 18:09:26 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// I did choose to use Unsplash API for the VG assignment. https://unsplash.com/developers
+
 //global variables
 const key = "C3esLyVT-9VL0nu9YTFTNSRD1EYXCa1UDPI_Fus3g8M";
 const btn = document.querySelector("#myBtn");
@@ -6,30 +8,30 @@ const randomBtn = document.querySelector("#random")
 btn.addEventListener("click", async () =>{
     const userInput = document.querySelector("#searchInput").value;
         try
-        {
-            const myFetch = await fetch(`https://api.unsplash.com/search/photos?&per_page=30&query=${userInput}&client_id=${key}`)
-            if(!myFetch.ok){
+     {
+        const myFetch = await fetch(`https://api.unsplash.com/search/photos?&per_page=30query=${userInput}&client_id=${key}`)
+        if(!myFetch.ok){
 
-                throw new Error(myFetch.status)
-            }
-                const data = await myFetch.json()
-                const results = data.results 
-                document.querySelector("#randomImg").innerHTML = " ";
+            throw new Error(myFetch.status)
+        }
+            const data = await myFetch.json()
+             const results = data.results 
+            document.querySelector("#randomImg").innerHTML = " ";
 
-                //loop through the object with a forEach loop
-                results.forEach(i => {
-                const myImages = i.urls.small
-                document.querySelector("#imageGrid").innerHTML += `
-                <img src="${myImages}" alt="">
-                `;
-            })
-        }
-        catch(error){
-            console.log(error)
-            //Finally clear the search input field after each search
-        } finally{
-            document.querySelector("#searchInput").value = " ";
-        }
+             //loop through the object with a forEach loop
+            results.forEach(i => {
+            const myImages = i.urls.small
+            document.querySelector("#imageGrid").innerHTML += `
+             <img src="${myImages}" alt="">
+             `;
+        })
+    }
+    catch(error){
+         console.log(error)
+         //Finally clear the search input field after each search
+    } finally{
+         document.querySelector("#searchInput").value = " ";
+    }
 
 })
 
