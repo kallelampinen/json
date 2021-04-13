2653a4e2ae3e4d0f66c82f7734da93792aa5f178
kallelampinen <71073159+kallelampinen@users.noreply.github.com>
2021-04-13 22:52:22 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -4,25 +4,24 @@
 const key = "C3esLyVT-9VL0nu9YTFTNSRD1EYXCa1UDPI_Fus3g8M";
 const btn = document.querySelector("#myBtn");
 const randomBtn = document.querySelector("#random")
-// fetch images with search throug input field.
+// fetch images with search throug input field. I did choose to not have a the static width and height in each grid cell to give the site a more dynamic feel and also to let each picture have the original size.
 btn.addEventListener("click", async () =>{
-    const userInput = document.querySelector("#searchInput").value;
+        const userInput = document.querySelector("#searchInput").value;
         try
         {
             const myFetch = await fetch(`https://api.unsplash.com/search/photos?&per_page=30&query=${userInput}&client_id=${key}`)
             if(!myFetch.ok){
-
                 throw new Error(myFetch.status)
-            }
+            } 
                 const data = await myFetch.json()
                 const results = data.results 
                 document.querySelector("#randomImg").innerHTML = " ";
-
+                document.querySelector("#imageGrid").innerHTML = " ";
                 //loop through the object with a forEach loop
                 results.forEach(i => {
                 const myImages = i.urls.small
                 document.querySelector("#imageGrid").innerHTML += `
-                <img src="${myImages}" alt="">
+                <img src="${myImages}" alt="" id="img1">
                 `;
             })
         }
@@ -32,7 +31,6 @@ btn.addEventListener("click", async () =>{
         } finally{
             document.querySelector("#searchInput").value = " ";
         }
-
 })
 
 //Fetch a random image
@@ -53,9 +51,10 @@ randomBtn.addEventListener("click", async () =>{
     <img src="${randomImg}">
     <p id="desc">"${descriptionImg}"</p>
     `;
+    console.log(descriptionImg)
     }
     catch(error){
 
         console.log(error)
-    }
+    } 
 })
