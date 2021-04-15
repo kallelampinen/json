d45ee251e0851cd1da4297e5c9dd31830b285983
kallelampinen <71073159+kallelampinen@users.noreply.github.com>
2021-04-15 18:17:16 +0200

    a

---
+++ js/config.js
@@ -0,0 +1,7 @@
+config = {
+    
+    key: "C3esLyVT-9VL0nu9YTFTNSRD1EYXCa1UDPI_Fus3g8M"
+
+}
+
+///test//////
--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@
 
 //global variables
 
-const key = config.key;
-
+const key = "C3esLyVT-9VL0nu9YTFTNSRD1EYXCa1UDPI_Fus3g8M"
 const btn = document.querySelector("#myBtn");
 const randomBtn = document.querySelector("#random")
 // fetch images with search throug input field. 
